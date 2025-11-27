04bf8a599a35b60c6d56df023f043283798520b9
Rudy Zeinoun <rudy.zeinoun@whitebeard.me>
2025-11-27 16:52:02 +0100

    Add MSO comments for padding between texts

--- packages/block-text/src/EmailMarkdown.tsx
+++ packages/block-text/src/EmailMarkdown.tsx
@@ -1,73 +1,73 @@
-import insane, { AllowedTags } from 'insane';
+// import insane, { AllowedTags } from 'insane';
 import { marked, Renderer } from 'marked';
 import React, { CSSProperties, useMemo } from 'react';
 
-const ALLOWED_TAGS: AllowedTags[] = [
-  'a',
-  'article',
-  'b',
-  'blockquote',
-  'br',
-  'caption',
-  'code',
-  'del',
-  'details',
-  'div',
-  'em',
-  'h1',
-  'h2',
-  'h3',
-  'h4',
-  'h5',
-  'h6',
-  'hr',
-  'i',
-  'img',
-  'ins',
-  'kbd',
-  'li',
-  'main',
-  'ol',
-  'p',
-  'pre',
-  'section',
-  'span',
-  'strong',
-  'sub',
-  'summary',
-  'sup',
-  'table',
-  'tbody',
-  'td',
-  'th',
-  'thead',
-  'tr',
-  'u',
-  'ul',
-];
-const GENERIC_ALLOWED_ATTRIBUTES = ['style', 'title'];
-const TABLE_ALLOWED_ATTRIBUTES = ['width', 'cellpadding', 'cellspacing', 'border', 'role'];
+// const ALLOWED_TAGS: AllowedTags[] = [
+//   'a',
+//   'article',
+//   'b',
+//   'blockquote',
+//   'br',
+//   'caption',
+//   'code',
+//   'del',
+//   'details',
+//   'div',
+//   'em',
+//   'h1',
+//   'h2',
+//   'h3',
+//   'h4',
+//   'h5',
+//   'h6',
+//   'hr',
+//   'i',
+//   'img',
+//   'ins',
+//   'kbd',
+//   'li',
+//   'main',
+//   'ol',
+//   'p',
+//   'pre',
+//   'section',
+//   'span',
+//   'strong',
+//   'sub',
+//   'summary',
+//   'sup',
+//   'table',
+//   'tbody',
+//   'td',
+//   'th',
+//   'thead',
+//   'tr',
+//   'u',
+//   'ul',
+// ];
+// const GENERIC_ALLOWED_ATTRIBUTES = ['style', 'title'];
+// const TABLE_ALLOWED_ATTRIBUTES = ['width', 'cellpadding', 'cellspacing', 'border', 'role'];
 const PARAGRAPH_TOP_PADDING_PX = 5;
 const PARAGRAPH_BOTTOM_PADDING_PX = 16;
 
-function sanitizer(html: string): string {
-  return insane(html, {
-    allowedTags: ALLOWED_TAGS,
-    allowedAttributes: {
-      ...ALLOWED_TAGS.reduce<Record<string, string[]>>((res, tag) => {
-        res[tag] = [...GENERIC_ALLOWED_ATTRIBUTES];
-        return res;
-      }, {}),
-      img: ['src', 'srcset', 'alt', 'width', 'height', ...GENERIC_ALLOWED_ATTRIBUTES],
-      table: [...TABLE_ALLOWED_ATTRIBUTES, ...GENERIC_ALLOWED_ATTRIBUTES],
-      td: ['align', 'width', ...GENERIC_ALLOWED_ATTRIBUTES],
-      th: ['align', 'width', ...GENERIC_ALLOWED_ATTRIBUTES],
-      a: ['href', 'target', ...GENERIC_ALLOWED_ATTRIBUTES],
-      ol: ['start', ...GENERIC_ALLOWED_ATTRIBUTES],
-      ul: ['start', ...GENERIC_ALLOWED_ATTRIBUTES],
-    },
-  });
-}
+// function sanitizer(html: string): string {
+//   return insane(html, {
+//     allowedTags: ALLOWED_TAGS,
+//     allowedAttributes: {
+//       ...ALLOWED_TAGS.reduce<Record<string, string[]>>((res, tag) => {
+//         res[tag] = [...GENERIC_ALLOWED_ATTRIBUTES];
+//         return res;
+//       }, {}),
+//       img: ['src', 'srcset', 'alt', 'width', 'height', ...GENERIC_ALLOWED_ATTRIBUTES],
+//       table: [...TABLE_ALLOWED_ATTRIBUTES, ...GENERIC_ALLOWED_ATTRIBUTES],
+//       td: ['align', 'width', ...GENERIC_ALLOWED_ATTRIBUTES],
+//       th: ['align', 'width', ...GENERIC_ALLOWED_ATTRIBUTES],
+//       a: ['href', 'target', ...GENERIC_ALLOWED_ATTRIBUTES],
+//       ol: ['start', ...GENERIC_ALLOWED_ATTRIBUTES],
+//       ul: ['start', ...GENERIC_ALLOWED_ATTRIBUTES],
+//     },
+//   });
+// }
 
 class CustomRenderer extends Renderer {
   private linkColor?: string | null;
@@ -98,15 +98,12 @@ ${body}</tbody>
   }
 
   paragraph(text: string) {
-    return `<table role="presentation" width="100%" cellpadding="0" cellspacing="0" border="0" style="word-break:break-word">
-<tbody>
-<tr>
-<td style="padding-top: ${PARAGRAPH_TOP_PADDING_PX}px; padding-bottom: ${PARAGRAPH_BOTTOM_PADDING_PX}px;">
-<p style="margin: 0;">${text}</p>
-</td>
-</tr>
-</tbody>
-</table>`;
+    return `<p style="margin-top: ${PARAGRAPH_TOP_PADDING_PX}px; margin-bottom: ${PARAGRAPH_BOTTOM_PADDING_PX}px;">${text}</p>
+<!--[if mso]>
+<table role="presentation" width="100%" cellpadding="0" cellspacing="0">
+  <tr><td height="${PARAGRAPH_BOTTOM_PADDING_PX}" style="line-height:${PARAGRAPH_BOTTOM_PADDING_PX}px;">&nbsp;</td></tr>
+</table>
+<![endif]-->`;
   }
 }
 
@@ -122,7 +119,8 @@ function renderMarkdownString(str: string, linkColor?: string | null): string {
   if (typeof html !== 'string') {
     throw new Error('marked.parse did not return a string');
   }
-  return sanitizer(html);
+  return html;
+  // return sanitizer(html);
 }
 
 type Props = {
